68b59ac434aabaaaeefb6b9b8e9f13d8e8dbff6e
Robert Dorna <rdorna8@gmail.com>
2022-07-13 19:44:57 +0200

    style: format diet screen

--- src/screens/diet/index.js
+++ src/screens/diet/index.js
@@ -1,35 +1,39 @@
-import React from 'react'
-import { ScrollView, StyleSheet, Text } from 'react-native'
-import { Screen } from '../../components/atoms'
-import { DatePicker, Nutrients, Items, Water } from '../../components/molecules'
-import { useDay } from '../../utils/cache'
-import { DIET_QUERY } from './graph';
-import { WithData } from '../../utils/with-data'
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { Screen } from "../../components/atoms";
+import {
+  DatePicker,
+  Nutrients,
+  Items,
+  Water,
+} from "../../components/molecules";
+import { useDay } from "../../utils/cache";
+import { DIET_QUERY } from "./graph";
+import { WithData } from "../../utils/with-data";
 import { useQuery } from "@apollo/client";
 
-
 export default function Diet() {
-  const day = useDay()
-  const useDietQuery = () => useQuery(
-    DIET_QUERY,
-    {
+  const day = useDay();
+  const useDietQuery = () =>
+    useQuery(DIET_QUERY, {
       variables: { day },
       // fetchPolicy: "network-only"
-    }
-  );
+    });
 
-  return (
-    <WithData Screen={DietScreen} useQuery={useDietQuery} />
-  )
+  return <WithData Screen={DietScreen} useQuery={useDietQuery} />;
 }
 
-function DietScreen({ data: { diet: { goal, water, mealTimes } } }) {
+function DietScreen({
+  data: {
+    diet: { goal, water, mealTimes },
+  },
+}) {
   return (
     <Screen>
       <DatePicker />
       <ScrollView style={styles.content}>
         <Water water={water} goal={goal} />
-        {mealTimes.map(({ hour, meal, eats }) =>
+        {mealTimes.map(({ hour, meal, eats }) => (
           <Items
             key={meal.name}
             hour={hour}
@@ -37,11 +41,11 @@ function DietScreen({ data: { diet: { goal, water, mealTimes } } }) {
             remark="13 PLN"
             eats={eats}
           />
-        )}
+        ))}
       </ScrollView>
       <Nutrients
         style={styles.summary}
-        fields={['kcal', 'protein', 'carb', 'fat', 'salt']}
+        fields={["kcal", "protein", "carb", "fat", "salt"]}
         items={mealTimes}
         goal={goal}
       />
@@ -51,11 +55,11 @@ function DietScreen({ data: { diet: { goal, water, mealTimes } } }) {
 
 const styles = StyleSheet.create({
   content: {
-    alignSelf: 'stretch'
+    alignSelf: "stretch",
   },
   summary: {
-    backgroundColor: '#e1e1e1',
+    backgroundColor: "#e1e1e1",
     borderTopWidth: 1,
-    borderColor: 'darkgreen',
+    borderColor: "darkgreen",
   },
-})
+});
