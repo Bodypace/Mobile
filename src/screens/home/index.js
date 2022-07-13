f6d984636fa8dd898a16253a82d52cea0c51d962
Robert Dorna <rdorna8@gmail.com>
2022-07-13 19:56:09 +0200

    style: format home screen

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,52 +1,39 @@
-import React from 'react'
-import { ScrollView, StyleSheet } from 'react-native'
-import { Screen } from '../../components/atoms'
-import { DatePicker, Nutrients, Items } from '../../components/molecules'
-import { useDay } from '../../utils/cache'
-import { HOME_QUERY } from './graph';
-import { WithData } from '../../utils/with-data'
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { Screen } from "../../components/atoms";
+import { DatePicker, Nutrients, Items } from "../../components/molecules";
+import { useDay } from "../../utils/cache";
+import { HOME_QUERY } from "./graph";
+import { WithData } from "../../utils/with-data";
 import { useQuery } from "@apollo/client";
 
-
 export default function Home() {
-  const day = useDay()
-  const useHomeQuery = () => useQuery(
-    HOME_QUERY,
-    {
+  const day = useDay();
+  const useHomeQuery = () =>
+    useQuery(HOME_QUERY, {
       variables: { day },
       // fetchPolicy: "network-only"
-    }
-  );
+    });
 
-  return (
-    <WithData Screen={HomeScreen} useQuery={useHomeQuery} />
-  )
+  return <WithData Screen={HomeScreen} useQuery={useHomeQuery} />;
 }
 
-function HomeScreen({ data: { home: { inventory, shoppingList, wasted, goal, mealTimes } } }) {
+function HomeScreen({
+  data: {
+    home: { inventory, shoppingList, wasted, goal, mealTimes },
+  },
+}) {
   return (
     <Screen>
       <DatePicker />
       <ScrollView style={styles.content}>
-        <Items
-          name="In Home"
-          remark="13 PLN"
-          eats={inventory}
-        />
-        <Items
-          name="Shopping List"
-          remark="13 PLN"
-          eats={shoppingList}
-        />
-        <Items
-          name="Wasted"
-          remark="13 PLN"
-          eats={wasted}
-        />
+        <Items name="In Home" remark="13 PLN" eats={inventory} />
+        <Items name="Shopping List" remark="13 PLN" eats={shoppingList} />
+        <Items name="Wasted" remark="13 PLN" eats={wasted} />
       </ScrollView>
       <Nutrients
         style={styles.summary}
-        fields={['kcal', 'protein', 'carb', 'fat', 'salt']}
+        fields={["kcal", "protein", "carb", "fat", "salt"]}
         items={mealTimes}
         goal={goal}
       />
@@ -56,11 +43,11 @@ function HomeScreen({ data: { home: { inventory, shoppingList, wasted, goal, mea
 
 const styles = StyleSheet.create({
   content: {
-    alignSelf: 'stretch'
+    alignSelf: "stretch",
   },
   summary: {
-    backgroundColor: '#e1e1e1',
+    backgroundColor: "#e1e1e1",
     borderTopWidth: 1,
-    borderColor: 'darkgreen',
+    borderColor: "darkgreen",
   },
-})
+});
