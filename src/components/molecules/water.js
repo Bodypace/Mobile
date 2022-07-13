a2c1eda85d0eefa22075f2f499597d17b37cd74e
Robert Dorna <rdorna8@gmail.com>
2022-07-13 19:45:40 +0200

    feat: format Water component and make goal optional (#2)

--- src/components/molecules/water.js
+++ src/components/molecules/water.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { ItemsHeader } from './items'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { ItemsHeader } from "./items";
 
+export default function Water({ water, goal }) {
+  const current = water.reduce((sum, glass) => sum + glass.amount, 0);
 
-export default function Water ({ water, goal }) {
-  const current = water.reduce((sum, glass) => sum + glass.amount, 0)
+  const remark = goal ? `${current} / ${goal.water} L` : `${current} L`;
 
   return (
     <View style={styles.container}>
-      <ItemsHeader
-        color="#2D9CDB"
-        name="Water"
-        remark={`${current} / ${goal.water} L`}
-      />
-      <View style={{alignItems: 'center'}}>
+      <ItemsHeader color="#2D9CDB" name="Water" remark={remark} />
+      <View style={{ alignItems: "center" }}>
         <Text style={styles.text}>Water</Text>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -25,6 +22,6 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
   text: {
-    fontSize: 24
-  }
-})
+    fontSize: 24,
+  },
+});
