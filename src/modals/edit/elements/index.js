45f915ffdd70ed12e938d941fa75035736e5b67b
Robert Dorna <rdorna8@gmail.com>
2022-07-14 17:28:27 +0200

    style: format edit modal Elements component

--- src/modals/edit/elements/index.js
+++ src/modals/edit/elements/index.js
@@ -1,31 +1,31 @@
-import { Field } from './field'
-import { Fields } from './fields'
-import { Legend } from './legend'
-import { Separator } from './separator'
-import { Title } from './title'
+import { Field } from "./field";
+import { Fields } from "./fields";
+import { Legend } from "./legend";
+import { Separator } from "./separator";
+import { Title } from "./title";
 
-
-export default function Elements ({ layout, item, edit, setEdit }) {
+export default function Elements({ layout, item, edit, setEdit }) {
   const elements = {
-    "title": Title,
-    "separator": Separator,
-    "legend": Legend,
-    "field": Field,
-    "fields": Fields,
-  }
+    title: Title,
+    separator: Separator,
+    legend: Legend,
+    field: Field,
+    fields: Fields,
+  };
 
-  return (
-    layout.map((element, index) => {
-      const type = Array.isArray(element) ? "fields" : element.type
-      const Element = elements[type]
-      return Element === undefined ? <></> :
-        <Element
-          key={index}
-          element={element}
-          item={item}
-          edit={edit}
-          setEdit={setEdit}
-        />
-    })
-  )
+  return layout.map((element, index) => {
+    const type = Array.isArray(element) ? "fields" : element.type;
+    const Element = elements[type];
+    return Element === undefined ? (
+      <></>
+    ) : (
+      <Element
+        key={index}
+        element={element}
+        item={item}
+        edit={edit}
+        setEdit={setEdit}
+      />
+    );
+  });
 }
